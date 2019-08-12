a6362650864f31cad389bb2216276290744017f6
John Hallat <jhallat@gmail.com>
2019-08-11 22:18:52 -0400

    initial commit

---
+++ src/main/java/com/jhallat/zookeeper/manager/ZookeeperManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.jhallat.zookeeper.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookeeperManagerServerApplication.class, args);
+	}
+
+}
