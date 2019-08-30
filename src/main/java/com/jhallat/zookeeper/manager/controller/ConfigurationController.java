6933587051a4db3e5702b72c90aa9fe97b490e32
John Hallat <jhallat@gmail.com>
2019-08-30 07:48:16 -0400

    update config service

--- src/main/java/com/jhallat/zookeeper/manager/controller/ConfigurationController.java
+++ src/main/java/com/jhallat/zookeeper/manager/controller/ConfigurationController.java
@@ -1,15 +1,20 @@
 package com.jhallat.zookeeper.manager.controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
+
+import com.jhallat.zookeeper.manager.model.Configuration;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -50,7 +55,33 @@ public class ConfigurationController {
 	}
 	
 	@GetMapping("/configuration/{config}")
-	public ResponseEntity<Configuration> getConfiguration() {
-		return null;
+	public ResponseEntity<Configuration> getConfiguration(@PathVariable("config") String config) {
+		
+		//TODO Add validation to config parameter
+		File file = new File(zookeeperHome + "/" + zookeeperConfiguration + "/" + config + ".cfg");
+		Configuration configuration = new Configuration();
+		if (file.exists()) {
+			try(Scanner scanner = new Scanner(file)) {
+				scanner.useDelimiter("\n");
+				while(scanner.hasNextLine()) {
+					String line = scanner.nextLine();
+					String[] values = line.split("=");
+					if (values.length != 2) {
+						log.warn("Invalid setting in configuration: " + config);
+						throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+					}
+					
+				}
+			} catch (IOException exception) {
+				log.error("Error reading configuration", exception);
+				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);				
+			}
+			
+		} else {
+			log.warn(config + " is not a valid configuration");
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<>(configuration, HttpStatus.OK);
 	}
 }
